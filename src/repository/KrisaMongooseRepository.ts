e6aae9704645ab816725a92fae114b00c0238041
Alex Andru <alex007d@gmail.com>
2023-12-24 00:14:53 +0100

    Add sort to getKrisas in KrisaRepository

--- src/repository/KrisaMongooseRepository.ts
+++ src/repository/KrisaMongooseRepository.ts
@@ -4,7 +4,7 @@ import { KrisaFromDb } from '@/types';
 
 class KrisaMongooseRepository implements KrisaRepository {
   getKrisas = async (): Promise<KrisaFromDb[]> => {
-    const krisas = await Krisa.find({}).lean();
+    const krisas = await Krisa.find({}).lean().sort({ krisaNumber: 'asc' });
     return krisas as KrisaFromDb[];
   };
 }
